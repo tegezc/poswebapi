9a51ea9301a69335daf37159d731502c8c817e62
suparman <tegesitt@gmail.com>
2024-09-28 04:57:27 +0700

    override method create agar leluasa

--- products/views.py
+++ products/views.py
@@ -2,8 +2,17 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Product
 from .serializers import ProductSerializer
+from rest_framework.response import Response
+from rest_framework import status
 
 class ProductListCreateView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        # Mengembalikan response dengan serializer.data yang sudah termasuk ID
+        return Response(serializer.data, status=status.HTTP_201_CREATED)

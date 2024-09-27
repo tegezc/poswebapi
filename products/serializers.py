7aff5f4fc639fa6add1c2c4772d4c619ec5396e8
suparman <tegesitt@gmail.com>
2024-09-28 04:56:32 +0700

    menambah field id pada response

--- products/serializers.py
+++ products/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ['name', 'nip', 'stock']
+        fields = ['id','name', 'nip', 'stock']

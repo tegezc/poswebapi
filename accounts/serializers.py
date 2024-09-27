f3c51a20cdda136248a62e90301e88c491fd6c9f
suparman <tegesitt@gmail.com>
2024-09-28 02:54:38 +0700

    add create user di serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -4,4 +4,17 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['name', 'email', 'phone_number', 'birth_date']
+        fields = ['email', 'name', 'phone_number', 'birth_date', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+        
+    def create(self, validated_data):
+        # Membuat instance user baru
+        user = User(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            phone_number=validated_data['phone_number'],
+            birth_date=validated_data['birth_date']
+        )
+        user.set_password(validated_data['password'])  # Hash password
+        user.save()  # Simpan user ke database
+        return user  # Kembalikan instance user yan

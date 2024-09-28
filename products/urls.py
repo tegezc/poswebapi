51ee3a269678607d1d5a0e26048e41cb77be605e
suparman <tegesitt@gmail.com>
2024-09-28 21:38:31 +0700

    modif path name for product app

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProductListCreateView
 
 urlpatterns = [
-    path('', ProductListCreateView.as_view(), name='product-list-create'),
+    path('', ProductListCreateView.as_view(), name='product'),
 ]
